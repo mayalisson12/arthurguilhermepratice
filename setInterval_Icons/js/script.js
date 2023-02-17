0425846e5ea9ad5fd56b31cb49b635830538cb62
unknown <arthurguilherme2013@gmail.com>
2023-02-17 12:51:20 +0000

    End of Little Project: setInterval Icons

--- setInterval_Icons/js/script.js
+++ setInterval_Icons/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+const icons = document.querySelectorAll(".container i");
+let i = 1;
+function showIcons() {
+    i++;
+    const icon = document.querySelector("i.change");
+    icon.classList.remove("change");
+
+    if(i > icons.length) {
+        icons[0].classList.add("change");
+        i = 1;
+    } else {
+        icon.nextElementSibling.classList.add("change");
+    }
+};
+
+setInterval(showIcons, 1000);
